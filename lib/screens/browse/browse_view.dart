b96b531f8f35ed4bc3b64093c69c561f817399e5
eslam <88260578+eslam301@users.noreply.github.com>
2023-11-04 18:52:29 +0200

    Finished Home view added main widgets Finished MoviesCategory View

--- lib/screens/browse/browse_view.dart
+++ lib/screens/browse/browse_view.dart
@@ -1,16 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:movie_application/layout/home_layout.dart';
+import 'package:movie_application/screens/home/home_view.dart';
 
-class BrowseView extends StatelessWidget {
+import 'Widgets/MoviesCategory.dart';
+
+class BrowseView extends StatefulWidget {
   const BrowseView({super.key});
 
   @override
+  State<BrowseView> createState() => _BrowseViewState();
+}
+
+class _BrowseViewState extends State<BrowseView> {
+  @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     return Scaffold(
-        body: Column(
-          children: [
-            Text("Browse View"),
-          ],
-        )
-    );
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          toolbarHeight: 90,
+          title: Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Text(
+              "Browse Category ",
+              style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+              textAlign: TextAlign.start,
+            ),
+          ),
+          backgroundColor: Colors.black,
+        ),
+        body: SingleChildScrollView(
+            child: Column(children: [
+          MoviesCategory(
+            text1: "Action",
+            title1: "assets/images/Action.png",
+            text2: "Adventure",
+            title2: "assets/images/Adventure.png",
+          ),
+          SizedBox(height: 30),
+          MoviesCategory(
+            text1: "Animation",
+            title1: "assets/images/Animation.png",
+            text2: "Comedy",
+            title2: "assets/images/Comedy.png",
+          ),
+          SizedBox(height: 30),
+          MoviesCategory(
+            text1: "Documentary",
+            title1: "assets/images/Documentary.png",
+            text2: "Crime",
+            title2: "assets/images/Crime.png",
+          ),
+          SizedBox(height: 30),
+          MoviesCategory(
+            text1: "Drama",
+            title1: "assets/images/Drama.png",
+            text2: "Family",
+            title2: "assets/images/Family.png",
+          ),
+          SizedBox(height: 30),
+          MoviesCategory(
+            text1: "Fantasy",
+            title1: "assets/images/Fantasy.png",
+            text2: "History",
+            title2: "assets/images/History.png",
+          ),
+          SizedBox(height: 30),
+          MoviesCategory(
+            text1: "Horror",
+            title1: "assets/images/Horror.png",
+            text2: "Music",
+            title2: "assets/images/Music.png",
+          ),
+          SizedBox(height: 30),
+          MoviesCategory(
+            text1: "Mystery",
+            title1: "assets/images/Mystery.png",
+            text2: "Romance",
+            title2: "assets/images/Romance.png",
+          ),
+          SizedBox(height: 30),
+          MoviesCategory(
+            text1: "Science\nFiction",
+            title1: "assets/images/ScienceFiction.png",
+            text2: "TV Movie",
+            title2: "assets/images/TvMovie.png",
+          ),
+          SizedBox(height: 30),
+          MoviesCategory(
+            text1: "Thriller",
+            title1: "assets/images/Thriller.png",
+            text2: "War",
+            title2: "assets/images/War.png",
+          ),
+          SizedBox(height: 30),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.only(right: 195),
+              height: 110,
+              child: Stack(
+                children: [
+                  Image.asset(
+                    "assets/images/Western.png",
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 40, left: 50),
+                    child: Text("Western",
+                        style: theme.textTheme.titleLarge!
+                            .copyWith(fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ])));
   }
 }
--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -1,18 +1,125 @@
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+import '../../widgets/book_mark_widget.dart';
+import '../../widgets/movie_small_panner_widget.dart';
+
+class HomeView extends StatefulWidget {
 
 
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  bool bookMarked = true;
+
+  @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     return Scaffold(
-      body: Column(
-        children: [
-          Text("Home View"),
-        ],
-      ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                clipBehavior: Clip.none,
+                alignment: Alignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/banner_movie.png",
+                  ),
+                  const Positioned(
+                    child: Icon(Icons.play_circle, size: 80, color: Colors.white,),
+
+                  ),
+                  Positioned(
+                    bottom: -40,
+                    left: 160,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Dora and the lost city of gold",
+                          style: theme.textTheme.bodyMedium?.copyWith(
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                          "Dora and the lost city of gold",
+                          style: theme.textTheme.bodySmall,
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: -80,
+                    left: 21,
+                    child: MovieSmallBanner(),
+                  ),
+
+                ]
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric( horizontal: 10,vertical: 20),
+                decoration: BoxDecoration(
+                  color: theme.colorScheme.secondary,
+                ),
+                width: double.infinity,
+                height: 250,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("New Releases Today", style: theme.textTheme.bodyLarge,),
+                    Expanded(
+                      child: ListView.builder(
+                        itemBuilder: (context, index) => const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: MovieSmallBanner(),
+                        ),
+                        itemCount: 10,
+                        scrollDirection: Axis.horizontal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric( horizontal: 10,vertical: 20),
+                decoration: BoxDecoration(
+                  color: theme.colorScheme.secondary,
+                ),
+                width: double.infinity,
+                height: 250,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("New Releases Today", style: theme.textTheme.bodyLarge,),
+                    Expanded(
+                      child: ListView.builder(
+                        itemBuilder: (context, index) => const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: MovieSmallBanner(),
+                        ),
+                        itemCount: 10,
+                        scrollDirection: Axis.horizontal,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              )
+            ],
+          ),
+        )
     );
   }
 }
