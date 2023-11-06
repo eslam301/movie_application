4f2290e0e3a3c2481d9a264f644f7348d5d847fe
Khaled <dolaadel9289@gmail.com>
2023-11-06 15:31:28 +0200

    Finished Home view added main widgets Finished BrowseCategory View

--- lib/screens/browse/Widgets/MoviesCategory.dart
+++ lib/screens/browse/Widgets/MoviesCategory.dart
@@ -1,57 +1,11 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+class Movies_Category {
+  final int categoreyId;
+  final String categoreytitle;
+  final String categoreyImage;
 
-class MoviesCategory extends StatelessWidget {
-  String text1;
-  String title1;
-  String text2;
-  String title2;
-
-  MoviesCategory(
-      {super.key,
-      required this.text1,
-      required this.title1,
-      required this.text2,
-      required this.title2});
-
-  @override
-  Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    return Container(
-        height: 110,
-        margin: EdgeInsets.only(left: 11),
-        child: Row(children: [
-          Stack(children: [
-            Image.asset(title1),
-            Padding(
-              padding: const EdgeInsets.only(top: 70, left: 60),
-              child: Text(
-                text1,
-                style: theme.textTheme.titleLarge!
-                    .copyWith(fontWeight: FontWeight.bold),
-              ),
-            ),
-          ]),
-          SizedBox(width: 15),
-          Container(
-              height: 110,
-              child: Row(
-                children: [
-                  Stack(
-                    children: [
-                      Image.asset(title2),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 80, left: 60),
-                        child: Text(
-                          text2,
-                          style: theme.textTheme.titleLarge!
-                              .copyWith(fontWeight: FontWeight.bold),
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              )),
-        ]));
-  }
+  Movies_Category({
+    required this.categoreyId,
+    required this.categoreytitle,
+    required this.categoreyImage,
+  });
 }
--- lib/screens/browse/browse_view.dart
+++ lib/screens/browse/browse_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:movie_application/layout/home_layout.dart';
-import 'package:movie_application/screens/home/home_view.dart';
-
+import 'package:movie_application/screens/browse/Widgets/MoviesCategoryItem.dart';
 import 'Widgets/MoviesCategory.dart';
 
 class BrowseView extends StatefulWidget {
@@ -12,114 +10,158 @@ class BrowseView extends StatefulWidget {
 }
 
 class _BrowseViewState extends State<BrowseView> {
+  List<Movies_Category> genres = [
+    Movies_Category(
+      categoreyId: 28,
+      categoreytitle: "Action",
+      categoreyImage: "assets/images/Action.png",
+    ),
+    Movies_Category(
+      categoreyId: 12,
+      categoreytitle: "Adventure",
+      categoreyImage: "assets/images/Adventure.png",
+    ),
+    Movies_Category(
+      categoreyId: 16,
+      categoreytitle: "Animation",
+      categoreyImage: "assets/images/Animation.png",
+    ),
+    Movies_Category(
+      categoreyId: 35,
+      categoreytitle: "Comedy ",
+      categoreyImage: "assets/images/Comedy.png",
+    ),
+    Movies_Category(
+      categoreyId: 80,
+      categoreytitle: "Documentary",
+      categoreyImage: "assets/images/Documentary.png",
+    ),
+    Movies_Category(
+      categoreyId: 99,
+      categoreytitle: "Crime",
+      categoreyImage: "assets/images/Crime.png",
+    ),
+    Movies_Category(
+      categoreyId: 18,
+      categoreytitle: "Drama",
+      categoreyImage: "assets/images/Drama.png",
+    ),
+    Movies_Category(
+      categoreyId: 10751,
+      categoreytitle: "Family",
+      categoreyImage: "assets/images/Family.png",
+    ),
+    Movies_Category(
+      categoreyId: 14,
+      categoreytitle: "Fantasy",
+      categoreyImage: "assets/images/Fantasy.png",
+    ),
+    Movies_Category(
+      categoreyId: 36,
+      categoreytitle: "History",
+      categoreyImage: "assets/images/History.png",
+    ),
+    Movies_Category(
+      categoreyId: 27,
+      categoreytitle: "Horror",
+      categoreyImage: "assets/images/Horror.png",
+    ),
+    Movies_Category(
+      categoreyId: 10402,
+      categoreytitle: "Music",
+      categoreyImage: "assets/images/Music.png",
+    ),
+    Movies_Category(
+      categoreyId: 9648,
+      categoreytitle: "Mystery",
+      categoreyImage: "assets/images/Mystery.png",
+    ),
+    Movies_Category(
+      categoreyId: 10749,
+      categoreytitle: "Romance",
+      categoreyImage: "assets/images/Romance.png",
+    ),
+    Movies_Category(
+      categoreyId: 878,
+      categoreytitle: "ScienceFiction",
+      categoreyImage: "assets/images/ScienceFiction.png",
+    ),
+    Movies_Category(
+      categoreyId: 10770,
+      categoreytitle: "Tv Movie",
+      categoreyImage: "assets/images/TvMovie.png",
+    ),
+    Movies_Category(
+      categoreyId: 53,
+      categoreytitle: "Thriller",
+      categoreyImage: "assets/images/Thriller.png",
+    ),
+    Movies_Category(
+      categoreyId: 10752,
+      categoreytitle: "War",
+      categoreyImage: "assets/images/War.png",
+    ),
+    Movies_Category(
+      categoreyId: 37,
+      categoreytitle: "Western",
+      categoreyImage: "assets/images/Western.png",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          toolbarHeight: 90,
-          title: Padding(
-            padding: const EdgeInsets.only(top: 30),
-            child: Text(
-              "Browse Category ",
-              style: TextStyle(
-                  fontSize: 22,
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.black,
+      ),
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            toolbarHeight: 100,
+            backgroundColor: Colors.black,
+            title: Padding(
+              padding: const EdgeInsets.only(left: 10, top: 30),
+              child: Text(
+                selectedCategory == null
+                    ? "Browse Category"
+                    : selectedCategory!.categoreytitle,
+                style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  color: Colors.white),
-              textAlign: TextAlign.start,
-            ),
-          ),
-          backgroundColor: Colors.black,
-        ),
-        body: SingleChildScrollView(
-            child: Column(children: [
-          MoviesCategory(
-            text1: "Action",
-            title1: "assets/images/Action.png",
-            text2: "Adventure",
-            title2: "assets/images/Adventure.png",
-          ),
-          SizedBox(height: 30),
-          MoviesCategory(
-            text1: "Animation",
-            title1: "assets/images/Animation.png",
-            text2: "Comedy",
-            title2: "assets/images/Comedy.png",
-          ),
-          SizedBox(height: 30),
-          MoviesCategory(
-            text1: "Documentary",
-            title1: "assets/images/Documentary.png",
-            text2: "Crime",
-            title2: "assets/images/Crime.png",
-          ),
-          SizedBox(height: 30),
-          MoviesCategory(
-            text1: "Drama",
-            title1: "assets/images/Drama.png",
-            text2: "Family",
-            title2: "assets/images/Family.png",
-          ),
-          SizedBox(height: 30),
-          MoviesCategory(
-            text1: "Fantasy",
-            title1: "assets/images/Fantasy.png",
-            text2: "History",
-            title2: "assets/images/History.png",
-          ),
-          SizedBox(height: 30),
-          MoviesCategory(
-            text1: "Horror",
-            title1: "assets/images/Horror.png",
-            text2: "Music",
-            title2: "assets/images/Music.png",
-          ),
-          SizedBox(height: 30),
-          MoviesCategory(
-            text1: "Mystery",
-            title1: "assets/images/Mystery.png",
-            text2: "Romance",
-            title2: "assets/images/Romance.png",
-          ),
-          SizedBox(height: 30),
-          MoviesCategory(
-            text1: "Science\nFiction",
-            title1: "assets/images/ScienceFiction.png",
-            text2: "TV Movie",
-            title2: "assets/images/TvMovie.png",
-          ),
-          SizedBox(height: 30),
-          MoviesCategory(
-            text1: "Thriller",
-            title1: "assets/images/Thriller.png",
-            text2: "War",
-            title2: "assets/images/War.png",
-          ),
-          SizedBox(height: 30),
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-              margin: EdgeInsets.only(right: 195),
-              height: 110,
-              child: Stack(
-                children: [
-                  Image.asset(
-                    "assets/images/Western.png",
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 70, left: 60),
-                    child: Text("Western",
-                        style: theme.textTheme.titleLarge!
-                            .copyWith(fontWeight: FontWeight.bold)),
-                  ),
-                ],
+                  fontSize: 22,
+                ),
               ),
             ),
           ),
-          SizedBox(
-            height: 20,
-          ),
-        ])));
+          body: selectedCategory == null
+              ? Padding(
+                  padding: const EdgeInsets.all(1),
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 1,
+                        crossAxisSpacing: 30,
+                        childAspectRatio: 9 / 7),
+                    itemBuilder: (context, index) => MoviesCategoryItem(
+                      movies_category: genres[index],
+                      index: index,
+                      onItemClick: onCategoryItemClick,
+                    ),
+                    itemCount: genres.length,
+                  ),
+                )
+              : Container(
+                  child: GestureDetector(
+                  onTap: () {},
+                ))),
+    );
+  }
+
+  Movies_Category? selectedCategory;
+
+  onCategoryItemClick(Movies_Category movies_category) {
+    print(movies_category.categoreyId);
+    selectedCategory = movies_category;
+    setState(() {});
   }
 }
